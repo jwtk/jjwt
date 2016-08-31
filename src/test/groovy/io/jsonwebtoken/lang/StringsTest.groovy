9735d1ad98678b15b04f65bd8f88daa32f6f5618
benoit <b.wiart@ubik-ingenierie.com>
2016-08-31 16:39:42 +0200

    improve jwt parser memory allocation
    re-use buffer instead of creating new ones
    avoid creating unneeded buffers in the Strings util methods
    Stop continuously copying array with StringBuilder#deleteCharAt
    work directly on StringBuilder instead of creating a temporary String
    
    test added to cover the modified methods

--- src/test/groovy/io/jsonwebtoken/lang/StringsTest.groovy
+++ src/test/groovy/io/jsonwebtoken/lang/StringsTest.groovy
@@ -14,4 +14,46 @@ class StringsTest {
         assertTrue Strings.hasText("  foo   ");
         assertTrue Strings.hasText("foo")
     }
+    
+    @Test
+    void testClean() {
+        assertEquals "this is a test", Strings.clean("this is a test")
+        assertEquals "this is a test", Strings.clean("   this is a test")
+        assertEquals "this is a test", Strings.clean("   this is a test   ")
+        assertEquals "this is a test", Strings.clean("\nthis is a test \t  ")
+        assertNull Strings.clean(null)
+        assertNull Strings.clean("")
+        assertNull Strings.clean("\t")
+        assertNull Strings.clean("      ")
+    }
+    
+    @Test
+    void testCleanCharSequence() {
+    	def result = Strings.clean(new StringBuilder("this is a test"))
+    	assertNotNull result
+        assertEquals "this is a test", result.toString()
+        
+        result = Strings.clean(new StringBuilder("   this is a test"))
+    	assertNotNull result
+        assertEquals "this is a test", result.toString()
+        
+        result = Strings.clean(new StringBuilder("   this is a test   "))
+    	assertNotNull result
+        assertEquals "this is a test", result.toString()
+        
+        result = Strings.clean(new StringBuilder("\nthis is a test \t  "))
+    	assertNotNull result
+        assertEquals "this is a test", result.toString()
+        
+        assertNull Strings.clean((StringBuilder) null)
+        assertNull Strings.clean(new StringBuilder(""))
+        assertNull Strings.clean(new StringBuilder("\t"))
+        assertNull Strings.clean(new StringBuilder("      "))
+    }
+    
+    
+    @Test
+    void testTrimWhitespace() {
+    	assertEquals "", Strings.trimWhitespace("      ")
+    }
 }
