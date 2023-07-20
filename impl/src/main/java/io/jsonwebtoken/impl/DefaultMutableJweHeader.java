65c9b4026a11778ae4658041376181e218ee4ae1
Les Hazlewood <121180+lhazlewood@users.noreply.github.com>
2023-07-19 22:32:46 -0700

    added license headers, removed unused Conjunctor interface concept

--- impl/src/main/java/io/jsonwebtoken/impl/DefaultMutableJweHeader.java
+++ impl/src/main/java/io/jsonwebtoken/impl/DefaultMutableJweHeader.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2023 jsonwebtoken.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jsonwebtoken.impl;
 
 import io.jsonwebtoken.MutableJweHeader;
@@ -13,6 +28,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * @param <T> return type for method chaining
+ * @since JJWT_RELEASE_VERSION
+ */
 public class DefaultMutableJweHeader<T extends MutableJweHeader<T>> implements MutableJweHeader<T> {
 
     protected final FieldMap params;
