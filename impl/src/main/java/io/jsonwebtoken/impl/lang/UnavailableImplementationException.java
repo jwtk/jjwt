ce6c55e8f30918242ffdc4f5986a7d1da67164b2
Les Hazlewood <121180+lhazlewood@users.noreply.github.com>
2023-09-16 16:42:41 -0700

    Closes #773
    Fixed exception message per recommendation.

--- impl/src/main/java/io/jsonwebtoken/impl/lang/UnavailableImplementationException.java
+++ impl/src/main/java/io/jsonwebtoken/impl/lang/UnavailableImplementationException.java
@@ -24,7 +24,8 @@ public final class UnavailableImplementationException extends RuntimeException {
 
     private static final String DEFAULT_NOT_FOUND_MESSAGE = "Unable to find an implementation for %s using " +
             "java.util.ServiceLoader. Ensure you include a backing implementation .jar in the classpath, " +
-            "for example jjwt-impl.jar, or your own .jar for custom implementations.";
+            "for example jjwt-jackson.jar, jjwt-gson.jar or jjwt-orgjson.jar, or your own .jar for " +
+            "custom implementations.";
 
     UnavailableImplementationException(final Class<?> klass) {
         super(String.format(DEFAULT_NOT_FOUND_MESSAGE, klass));
